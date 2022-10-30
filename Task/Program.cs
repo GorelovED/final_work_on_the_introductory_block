0213c4c4f9a68e501c06b66816fd33c3cb67783d
acer-pc-evgeniy <e.v.gen.gorelov@yandex.ru>
2022-10-30 18:08:59 +0400

    Написание кода

---
+++ Task/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Сколько элементов Вы хотите ввести? ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] arrayOne = new string[size];
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($"Введите {i+1}-й элемент: ");
+    string element = Convert.ToString(Console.ReadLine());
+    arrayOne[i] = element;
+}
+
+string[] arrayTwo = new string[size];
+int len = 3;
+int pos = 0;
+
+for (int j = 0; j < size; j++)
+{
+    if (arrayOne[j].Length <= len)
+    {
+        arrayTwo[pos] = arrayOne[j];
+        pos++;
+    }
+}
+
+Console.WriteLine($"[{string.Join(", ", arrayTwo)}] -> [{string.Join(", ",arrayTwo )}]");
